3bf9d60564cb66b24f886c8f68944e80384596e6
libb <bleeccc@gmail.com>
2015-03-17 11:37:06 +0800

    Initial Commit

---
+++ MVNO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVNO
+//
+//  Created by beautyx on 15-3-17.
+//  Copyright (c) 2015年 libb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVNO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVNO
+//
+//  Created by beautyx on 15-3-17.
+//  Copyright (c) 2015年 libb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
